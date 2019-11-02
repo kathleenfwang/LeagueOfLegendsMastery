968d0f5461a8f61a45b761305d49bb1c7518caf4
Kathleen Wang <kathleenchibimaru@gmail.com>
2019-11-02 13:10:35 -0400

    added champion information list

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const {make_API_call} = require('./API_helper')
+const leagueChampions = require('./leagueChampions')
 const app = express()
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000;
 
-const API_KEY = 'RGAPI-054a0139-18e3-4f4a-a17d-78c61a192531'
+const API_KEY = 'RGAPI-24294f19-9cec-49bb-b702-6c5eea1a6104'
 let url = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/terracemouse?api_key=' + API_KEY 
 
-console.log("TESTING TESTING TEST")
 //view engine to ejs: 
 app.set('view engine','ejs'); 
  
@@ -30,17 +30,56 @@ app.get('/summonerIDs', (req, res,next) => {
     		let champions = response2
         	console.log('sending response2')
         	let noChest = []  
+            let yesChest = [] 
         	let masteryCount = 0; 
  			for (let i=0;i<champions.length;i++ ) {  
 	 			if (!champions[i].chestGranted) noChest.push(champions[i].championId)  
-
+                if (champions[i].chestGranted) yesChest.push(champions[i].championId)
 	 			if (champions[i].championLevel == 7) { masteryCount++ }
 	 		}
 	 		console.log(noChest)
+            let leagueObj = leagueChampions.data
+            let leagueArr = Object.keys(leagueObj)
+            let first = leagueArr[0] // 'Aatrox'
+            console.log(leagueObj[first].key)
+            //[ {name:key}]
 
-	 		res.render('champions', {champions:champions, noChest:noChest, masteryCount:masteryCount})
-	 		return noChest
+             let b = {} 
+            for (let name of leagueArr ) {
+                   
+                b[leagueObj[name].key] = name
+                                             
+                 
+            }
+            let noChestChamps = [] 
+            let yesChestChamps = [] 
+            //newArr = [ {'500':Aatrox}, {'1': 'Annie'}]
+            //noChest = [3,5,16,50,...]
+
+
+           for (let val in noChest) {
+          noChestChamps.push((b[noChest[val].toString()]))
+
+          }
+           for (let val in yesChest) {
+          yesChestChamps.push((b[yesChest[val].toString()]))
+
+          }
+          noChestChamps = noChestChamps.filter ( x =>{
+            return x
+          })
+          yesChestChamps = yesChestChamps.filter ( x =>{
+            return x
+          })
+          noChestChamps = noChestChamps.sort()
+          yesChestChamps = yesChestChamps.sort()
+            console.log(yesChest)
+           
+             
+	 		res.render('champions', { sumID:sumID, champions:champions, noChest:noChest, masteryCount:masteryCount,noChestChamps:noChestChamps, yesChestChamps:yesChestChamps })
+	 		 
 	})
+
  
     .catch(next)
 })
